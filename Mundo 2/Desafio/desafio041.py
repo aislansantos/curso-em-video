d674bb24d242d477e62dfbd6b34bb1da77caf017
Aislan Santos <aislan_85@yahoo.com.br>
2021-03-14 22:46:55 -0300

    Correção Desafio041

--- Mundo 2/Desafio/desafio041.py
+++ Mundo 2/Desafio/desafio041.py
@@ -10,23 +10,26 @@ nascimento de um atleta e mostre sua categoria, de acordo com a idade:
 '''
 import os #Importa biblioteca do sistema
 os.system('cls' if os.name == 'nt' else 'clear') # limpa a tela na inicialização do programa
+from datetime import date
 
 print()
 print('-='*25 + ' Classificação de Atletas' + '=-'*25)
 print()
 
-idade = int(input('Digite a idade do Atleta: '))
+ano_nascimento = int(input('Digite o ano de nascimento do Atleta: '))
+ano_atual = date.today().year
+idade = ano_atual - ano_nascimento
 
 if idade <= 9:
-    print('Esse atleta é MIRIM')
+    print('Esse atleta tem {} anos, ele é MIRIM'.format(idade))
 elif idade <= 14:
-    print('Esse atleta é INFANTIL')
+    print('Esse atleta tem {} anos, ele é INFANTIL'.format(idade))
 elif idade <= 19:
-    print('Esse atleta é JÚNIOR')
+    print('Esse atleta tem {} anos, ele é JÚNIOR'.format(idade))
 elif idade <= 25:
-    print('Esse atleta é SÊNIOR')
+    print('Esse atleta tem {} anos, ele é SÊNIOR'.format(idade))
 else:
-    print('Esse atleta é MASTER')
+    print('Esse atleta tem {} anos, ele é MASTER'.format(idade))
 
 print()
 print('-='*25 + ' FIM' + '=-'*25)
