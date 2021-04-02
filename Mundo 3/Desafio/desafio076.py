4c36bb1acf0a3a311ff264ae89df1a815d3d228b
Aislan Santos <aislan_85@yahoo.com.br>
2021-04-01 21:26:48 -0300

    exercicio 76 terminado

--- Mundo 3/Desafio/desafio076.py
+++ Mundo 3/Desafio/desafio076.py
@@ -10,7 +10,27 @@ print()
 print('{:-^150}'.format(' Exercicio '))
 print()
 
-produtos = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.00)
+produtos = ('Lápis', 1.75, 'Borracha', 2, 'Caderno', 15.00, 'apontador do mikey', 5.50)
+
+print('-'*50)
+print('{:^50}'.format('LISTAGEM DE PREÇO'))
+print('-'*50)
+print()
+# tratando a tupla por posição(numero de index)
+for i in range(0, len(produtos)):
+    if i % 2 == 0 :
+         print('{:.<40} R$'.format(produtos[i]), end=' ')
+    elif i % 2 == 1 :
+        print('{: >5.2f}'.format(produtos[i]))
+print()
+print('-'*50)
+
+# tratando pelo tipo do dado string ou float/string
+for i in range(0, len(produtos)):
+    if type(produtos[i]) == str:
+        print('{:.<40} R$'.format(produtos[i]), end=' ')
+    else:
+        print('{: >5.2f}'.format(produtos[i]))
 
 
 print()
